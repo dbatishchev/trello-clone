fbf09cb8c150ff4c5f8487efaaa9aa29cc1f7dd4
Dmitrii <dbatishchev@gmail.com>
2018-04-04 13:40:52 +0300

    add boards listing to Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import map from 'lodash/map';
 import './Dashboard.scss';
 
 export default class Dashboard extends Component {
@@ -26,97 +27,27 @@ export default class Dashboard extends Component {
                   </div>
                   <div className="js-section-content">
                     <ul className="sidebar-boards-list js-board-list">
-                      <li className="compact-board-tile">
-                        <div className="compact-board-tile-wrapper">
-                          <span className="compact-board-tile-background" />
-                          <span className="compact-board-tile-fade" />
-                          <a className="js-open-board compact-board-tile-link dark" href="/b/rME98TvM/%D0%B4%D0%B5%D0%BB%D0%B0">
-                            <span className="compact-board-tile-link-thumbnail" />
-                            <span className="compact-board-tile-link-text details">
-                              <span className="compact-board-tile-link-text-name" title="Дела">Дела</span>
+                      {map(this.props.boards, board => (
+                        <li className="compact-board-tile">
+                          <div className="compact-board-tile-wrapper">
+                            <span className="compact-board-tile-background" />
+                            <span className="compact-board-tile-fade" />
+                            <a className="js-open-board compact-board-tile-link dark" href="/b/rME98TvM/%D0%B4%D0%B5%D0%BB%D0%B0">
+                              <span className="compact-board-tile-link-thumbnail" />
+                              <span className="compact-board-tile-link-text details">
+                              <span className="compact-board-tile-link-text-name">{board.title}</span>
                             </span>
-                            <span className="compact-board-tile-link-options">
+                              <span className="compact-board-tile-link-options">
                               <span className="compact-board-tile-link-options-unread js-unread" title="There is new activity on this board." />
                               <span className="icon-sm icon-close js-remove-board compact-board-tile-link-options-remove"
                                     title="Click to remove this board from your recent boards." />
                               <span className="icon-sm icon-star compact-board-tile-link-options-star js-star-board"
                                     title="Click to star this board. It will show up at top of your boards list." />
                             </span>
-                          </a>
-                        </div>
-                      </li>
-                      <li className="compact-board-tile">
-                        <div className="compact-board-tile-wrapper">
-                          <span className="compact-board-tile-background" />
-                          <span className="compact-board-tile-fade" />
-                            <a className="js-open-board compact-board-tile-link dark" href="/b/70dtwYYB/ololo-board">
-                              <span className="compact-board-tile-link-thumbnail" />
-                              <span className="compact-board-tile-link-text details has-sub-name">
-                                <span className="compact-board-tile-link-text-name" title="Ololo board">Ololo board</span>
-                                <span className="compact-board-tile-link-text-sub-name" title="Cutwise team">Cutwise team</span>
-                              </span>
-                              <span className="compact-board-tile-link-options">
-                                <span className="compact-board-tile-link-options-unread js-unread" title="There is new activity on this board." />
-                                <span className="icon-sm icon-close js-remove-board compact-board-tile-link-options-remove"
-                                      title="Click to remove this board from your recent boards." />
-                                <span className="icon-sm icon-star compact-board-tile-link-options-star js-star-board"
-                                      title="Click to star this board. It will show up at top of your boards list." />
-                              </span>
                             </a>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="js-all-boards">
-                <div className="">
-                  <div className="sidebar-section-header quiet js-sidebar-section-header bold"><span className="icon-sm icon-board sidebar-section-header-icon"></span><span
-                    className="sidebar-section-header-title">Personal Boards</span><span
-                    className="icon-sm icon-remove sidebar-section-header-toggle-icon dark-hover js-collapse-section"></span><span
-                    className="icon-sm icon-add sidebar-section-header-toggle-icon dark-hover js-expand-section hide"></span></div>
-                  <div className="js-section-content">
-                    <div className="js-starred-empty-state sidebar-starred-boards-empty-state hide"></div>
-                    <ul className="sidebar-boards-list js-board-list">
-                      <li className="compact-board-tile">
-                        <div className="compact-board-tile-wrapper"><span className="compact-board-tile-background"></span><span
-                          className="compact-board-tile-fade"></span><a className="js-open-board compact-board-tile-link dark" href="/b/vW0Qrc2T/%D0%B4%D0%B5%D0%BB%D0%B0"><span
-                          className="compact-board-tile-link-thumbnail"></span><span className="compact-board-tile-link-text details"><span
-                          className="compact-board-tile-link-text-name" title="Дела">Дела</span></span><span className="compact-board-tile-link-options"><span
-                          className="compact-board-tile-link-options-unread js-unread" title="There is new activity on this board."></span><span
-                          className="icon-sm icon-star compact-board-tile-link-options-star js-star-board"
-                          title="Click to star this board. It will show up at top of your boards list."></span></span></a></div>
-                      </li>
-                      <li className="compact-board-tile">
-                        <div className="compact-board-tile-wrapper"><span className="compact-board-tile-background"></span><span
-                          className="compact-board-tile-fade"></span><a className="js-open-board compact-board-tile-link dark" href="/b/rME98TvM/%D0%B4%D0%B5%D0%BB%D0%B0"><span
-                          className="compact-board-tile-link-thumbnail"></span><span className="compact-board-tile-link-text details"><span
-                          className="compact-board-tile-link-text-name" title="Дела">Дела</span></span><span className="compact-board-tile-link-options"><span
-                          className="compact-board-tile-link-options-unread js-unread" title="There is new activity on this board."></span><span
-                          className="icon-sm icon-star compact-board-tile-link-options-star js-star-board"
-                          title="Click to star this board. It will show up at top of your boards list."></span></span></a></div>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div>
-                  <div className="sidebar-section-header quiet js-sidebar-section-header bold"><a className="dark-hover" href="/cutwiseteam"><span
-                    className="icon-sm icon-organization sidebar-section-header-icon"></span><span className="sidebar-section-header-title mod-clickable">Cutwise team</span><span
-                    className="icon-sm icon-business-className sidebar-section-header-bc-icon hide js-has-business-className"></span></a><span
-                    className="icon-sm icon-remove sidebar-section-header-toggle-icon dark-hover js-collapse-section"></span><span
-                    className="icon-sm icon-add sidebar-section-header-toggle-icon dark-hover js-expand-section hide"></span></div>
-                  <div className="js-section-content">
-                    <div className="js-starred-empty-state sidebar-starred-boards-empty-state hide"></div>
-                    <ul className="sidebar-boards-list js-board-list">
-                      <li className="compact-board-tile">
-                        <div className="compact-board-tile-wrapper"><span className="compact-board-tile-background"></span><span
-                          className="compact-board-tile-fade"></span><a className="js-open-board compact-board-tile-link dark" href="/b/70dtwYYB/ololo-board"><span
-                          className="compact-board-tile-link-thumbnail"></span><span className="compact-board-tile-link-text details"><span
-                          className="compact-board-tile-link-text-name" title="Ololo board">Ololo board</span></span><span className="compact-board-tile-link-options"><span
-                          className="compact-board-tile-link-options-unread js-unread" title="There is new activity on this board."></span><span
-                          className="icon-sm icon-star compact-board-tile-link-options-star js-star-board"
-                          title="Click to star this board. It will show up at top of your boards list."></span></span></a></div>
-                      </li>
+                          </div>
+                        </li>
+                      ))}
                     </ul>
                   </div>
                 </div>
--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -1,8 +1,15 @@
 import {connect} from 'react-redux'
+import merge from 'lodash/merge'
+import values from 'lodash/values'
 import Dashboard from "../components/Dashboard/Dashboard";
 import {pinDashboard, unpinDashboard} from "../actions/index";
 
+const getBoardsList = (boardsState) => {
+  return values(merge(boardsState.boardsById, boardsState.boardsDetailsById));
+};
+
 const mapStateToProps = state => ({
+  boards: getBoardsList(state.boards),
   isPinned: state.dashboard.isPinned,
   isOpened: state.dashboard.isOpened,
 });
